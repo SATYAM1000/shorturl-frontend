a83d48f4507477aa56f6fbb6b6a3c001e0f18ec0
satyamx <satyamsingh748846@gmail.com>
2024-10-05 21:04:37 +0530

    done

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,137 @@
-import Image from "next/image";
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Clipboard, MoveRight, Loader, Copy } from "lucide-react";
+import axios from "axios";
+import { toast } from "sonner";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [loading, setLoading] = useState(false);
+  const [url, setUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [copying, setCopying] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const validateUrl = (url) => {
+    try {
+      new URL(url);
+      return true;
+    } catch (e) {
+      return false;
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setErrorMessage(""); // Clear any previous error messages
+    setLoading(true);
+
+    // Validate URL before proceeding
+    if (!validateUrl(url)) {
+      setErrorMessage("Please enter a valid URL.");
+      setLoading(false);
+      return;
+    }
+    try {
+      const response = await axios.post(
+        `https://url.dokopi.com/api/v1/url/create`,
+        {
+          originalURL: url,
+        }
+      );
+      setShortUrl(response.data.data);
+    } catch (error) {
+      console.error("Error shortening URL:", error.response.data);
+      toast.error(error.response.data.message || "Something went wrong");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleCopy = () => {
+    setCopying(true);
+    navigator.clipboard.writeText(shortUrl);
+    toast.success("Copied to clipboard");
+    setTimeout(() => {
+      setCopying(false);
+    }, 500);
+  };
+
+  return (
+    <main className="flex items-center justify-center w-full h-screen p-5 md:p-10">
+      <div className="w-full rounded-2xl md:w-[450px] p-5 md:p-8 border border-white/50 flex flex-col gap-5">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2.5">
+            <Label className="text-sm text-white">
+              Enter URL to shorten:{" "}
+              <span className="text-xs text-red-500">*</span>
+            </Label>
+            <div className="flex flex-col gap-2">
+              <Input
+                type="text"
+                placeholder="https://example.com"
+                className="text-white"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+                required
+              />
+              {errorMessage && (
+                <p className="text-xs text-red-500">{errorMessage}</p>
+              )}
+            </div>
+          </div>
+
+          <Button
+            type="submit"
+            disabled={loading}
+            className="w-full mt-2 transition-all duration-300 bg-blue-500 hover:bg-blue-700"
           >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+            {loading ? (
+              <>
+                <span className="text-sm">Generating Short URL</span>
+                <Loader className="w-4 h-4 ml-2 animate-spin" />
+              </>
+            ) : (
+              <>
+                <span className="text-sm">Shorten URL</span>
+                <MoveRight className="w-4 h-4 ml-2" />
+              </>
+            )}
+          </Button>
+
+          <div className="flex flex-col gap-2.5 mt-4">
+            <Label className="text-sm text-white">Generated URL:</Label>
+            <div className="flex items-center gap-2">
+              <Input
+                type="text"
+                placeholder="https://url.dokopi.com/..."
+                value={`https://url.dokopi.com/${shortUrl}`}
+                className="text-white"
+                readOnly
+              />
+              <button
+                type="button"
+                className="px-1 py-2 rounded-lg"
+                onClick={handleCopy}
+                disabled={!shortUrl}
+              >
+                {copying ? (
+                  <>
+                    <Loader className="w-4 h-4 ml-2 text-white animate-spin" />
+                  </>
+                ) : (
+                  <>
+                    <Clipboard className="w-5 h-5 ml-2 text-white" />
+                  </>
+                )}
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </main>
   );
 }
